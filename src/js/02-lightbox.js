b9ad72a24a0d0b23521b3d17d4e879f605b33177
Filip Vukić <filipvukic90@gmail.com>
2023-02-28 21:57:29 +0100

    finishing 2nd part

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+// adding markup
+
+const markup = galleryItems
+  .map(
+    (galleryItem) =>
+      `<a class="gallery__link" href="${galleryItem.original}">
+    <img
+      class="gallery__image"
+      src="${galleryItem.preview}"
+      alt="${galleryItem.description}"
+    /></a>`
+  )
+  .join("");
+
+console.log(markup);
+
+gallery.insertAdjacentHTML("afterbegin", markup);
+
+// adding modals
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captions: true,
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
