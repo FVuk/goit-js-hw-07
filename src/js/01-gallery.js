89e34a86648bf929482124348d17ecc757491f53
Filip Vukić <filipvukic90@gmail.com>
2023-02-28 21:41:46 +0100

    finishing 1st part

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,59 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+// adding markup
+
+const markup = galleryItems
+  .map(
+    (galleryItem) =>
+      `<div class="gallery__item">
+  <a class="gallery__link" href="${galleryItem.original}">
+    <img
+      class="gallery__image"
+      src="${galleryItem.preview}"
+      data-original="${galleryItem.original}"
+      alt="${galleryItem.description}"
+    />
+  </a>
+</div>`
+  )
+  .join("");
+
+console.log(markup);
+
+gallery.insertAdjacentHTML("afterbegin", markup);
+
+// preventing default settings
+
+gallery.addEventListener("click", (e) => {
+  e.preventDefault();
+  selectImage(e);
+});
+
+// adding modals
+
+function selectImage(e) {
+  if (e.target.nodeName !== "IMG") {
+    return;
+  } else {
+    const instance = basicLightbox.create(
+      `<img src="${e.target.dataset.original}">`,
+      { onClose: () => document.removeEventListener("keydown", closeImage) }
+    );
+
+    instance.show();
+
+    // bonus - closing modal via keyboard
+
+    const closeImage = (e) => {
+      if (e.code === "Escape") {
+        instance.close();
+      }
+    };
+    document.addEventListener("keydown", closeImage);
+  }
+}
